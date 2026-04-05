1e4dfc565d658a37d62160cb3aff895023bac9fe
Prince Kumar <pk0834284@gmail.com>
2026-04-05 19:03:51 +0530

    Ex2

---
+++ Day 1 (Java Basics)/Exercise/Ex2.java
@@ -0,0 +1,28 @@
+//Calculating the Area of a Rectangle
+//This exercise combines the concepts to build a small program.
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        double width = 0;
+        double height = 0;
+        double area = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the width: ");
+        width = scanner.nextDouble();
+
+        System.out.print("Enter the height: ");
+        height = scanner.nextDouble();
+
+        area = width * height;
+
+        // Note: For the superscript 2 (cm²), use Alt+0178 on Windows numpad
+        System.out.println("The area is " + area + "cm²");
+
+        scanner.close();
+    }
+}
