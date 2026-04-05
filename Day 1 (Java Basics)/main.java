2a3f053f37bda092d1a6ba432324b36d9b414c09
Prince Kumar <pk0834284@gmail.com>
2026-04-05 17:28:49 +0530

    Create main.java

---
+++ Day 1 (Java Basics)/main.java
@@ -0,0 +1,19 @@
+// What is Java : Java is a popular programming language used to build:
+//Android apps 
+//Web applications 
+// Desktop software 
+// Games 
+
+// First Java Program
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        
+    }
+}
+
+/* Explanation (Simple)
+   public class Main → Class name (Java starts from class)
+   main() → Starting point of program
+   System.out.println() → Prints output */
