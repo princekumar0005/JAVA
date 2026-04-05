c5c420a8b1f614e32778a0654f36e71a3b12585d
Prince Kumar <pk0834284@gmail.com>
2026-04-05 17:44:15 +0530

    main.java

--- Day 1 (Java Basics)/main.java
+++ Day 1 (Java Basics)/main.java
@@ -9,6 +9,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("I am Prince ");
+        System.out.println("I'm college student");
         
     }
 }
