30c6cf299c2aff26e9a30facd7c4aaf104a159ee
Prince Kumar <pk0834284@gmail.com>
2026-04-08 22:21:28 +0530

    Create Nested_switch.java

---
+++ Day 3 Control statements/Nested_switch.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        int j = 5;
+        int k = 1;
+
+        switch (j) {
+            case 4:
+                System.out.println("j is four");
+                break;
+            case 5:
+                System.out.println("j is five");
+                // Nested switch inside case 5
+                switch (k) {
+                    case 1:
+                        System.out.println("k is one");
+                        break;
+                    case 2:
+                        System.out.println("k is two");
+                        break;
+                }
+                break;
+            default:
+                System.out.println("j is not four or five");
+                break;
+        }
+    }
+}
+
