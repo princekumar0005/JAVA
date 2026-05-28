7cbaab66745514cc3dc3e50d261599b14b60b3d7
Prince Kumar <pk0834284@gmail.com>
2026-05-28 22:27:32 +0530

    Add abstract Student class with basic properties

---
+++ OOP/Mini Projects/StudentManagementSystem/Student.java
@@ -0,0 +1,32 @@
+abstract class Student {
+
+    private int id;
+    private String name;
+    private double marks;
+
+    Student(int id, String name, double marks) {
+        this.id = id;
+        this.name = name;
+        this.marks = marks;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getMarks() {
+        return marks;
+    }
+
+    abstract String grade();
+
+    public void display() {
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Marks: " + marks);
+    }
+}
